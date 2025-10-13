0ea062aa43e820fd56c4b747934a0c00ff683619
Bhavani Saladi <161202039+bhavani3420@users.noreply.github.com>
2025-10-13 11:40:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["qv97c2-5173.csb.app"], // 👈 add your host here
+  },
 })
